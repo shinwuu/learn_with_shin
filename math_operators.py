557193fc431065961dfcaaeda5303f1517a0643c
unknown <eunjingshun@hotmail.com>
2023-06-17 07:59:42 -0400

    python code

---
+++ math_operators.py
@@ -0,0 +1,23 @@
+'''
+Shinwuu
+Date 06/17/2023
+
+Summary:
+This code is to help learn the Jenkins CI/CD Pipeline
+
+Note:
+It will consist of basic math functions
+
+'''
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
---
+++ test_math_operators.py
@@ -0,0 +1,23 @@
+'''
+Shinwuu
+Date 06/17/2023
+
+Summary:
+This code is to help learn the Jenkins CI/CD Pipeline
+Unit Tests are extremely important in the SSDLC
+
+'''
+
+from math_operators import *
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(5,3) == 2
+
+def test_multiply():
+    assert multiply(2,3) == 6
+
+def test_divide():
+    assert divide(10,5) == 2
